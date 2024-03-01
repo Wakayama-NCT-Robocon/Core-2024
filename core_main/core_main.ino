0676a5b13fc81a44835ec45a8ed3daced4b097e6
gpgpc0 <tomonori.24681021@gmail.com>
2024-03-01 15:24:27 +0900

    Update core_main.ino

--- core_main/core_main.ino
+++ core_main/core_main.ino
@@ -1,7 +1,5 @@
 /* 2020/11/04 2018b24 竹中翔子
-/* 2024/01/13 2021a21 髙田陸生
-   モータ、サーボ、ソレノイドのテスト用プログラム
-   ArduinoDue用
+   2024/01/13 2021a21 髙田陸生
    2022/9/23 2021a21 髙田陸生　　LtoH関数の不具合を修正
 
    Esp32用
@@ -16,7 +14,8 @@
 */
 #include <PS4Controller.h>
 #include <stdio.h>
-#include<math.h>//数学関数
+#include <math.h>//数学関数
+#include <ESP32Servo.h>
 
 //シリアル送受信用定義
 #define DATA_SEND_NUMBER 17//送信データ数
@@ -24,8 +23,13 @@
 
 //シリアル送受信用グローバル変数
 int send_data[DATA_SEND_NUMBER] = {0}; //MDなどへの送信データ数
-//切り替え変数
-int SWM = 0;
+//フラグ変数
+//オブジェクト
+Servo Camera;
+
+//PIN設定
+const int PIN_SV = 5;//サーボの信号線
+
 typedef union {
   int8_t signed_data;
   uint8_t unsigned_data;
@@ -73,12 +77,14 @@ void setup() {
           ESP_bt_mac[5]); //esp32のマックアドレスを文字列に変換してる。
 
   PS4.begin(DS4_bt_mac);
+  Camera.attach(PIN_SV, 500, 2500);
+  Camera.write(0);
 }
 
 void loop() {
   int i, j;
   static int angle = 0;
-  int LStickY, LStickX, RStickY, RStickX, L1, L2, UP, DOWN, SQUARE, CIRCLE, TRIANGLE, LEFT, RIGHT, CROSS, R1, R2;
+  int LStickY, LStickX, RStickY, RStickX, L1, L2, UP, DOWN, SQUARE, CIRCLE, TRIANGLE, LEFT, RIGHT, CROSS, R1, R2, OPTION;
   int lY, lX, rX, rY;
   double pwm[5], x, y, Fire = 1.0;
   static double pwmLast[5];
@@ -134,6 +140,8 @@ void loop() {
     CROSS = PS4.Cross();
     RIGHT = PS4.Right();
     LEFT = PS4.Left();
+    OPTION = PS4.Options();
+
     if (-20 < LStickX && LStickX < 20) {
       LStickX = 0;
     }
@@ -149,70 +157,69 @@ void loop() {
     /*****主にいじる所ここから*****/
     //モーター
 
-    lY = LStickY * 0.13;
-    lX = LStickX * 0.13;
-    rX = RStickX * 0.13;
+    lY = LStickY * 0.13 * 0.8;
+    lX = LStickX * 0.13 * 0.8;
+    rX = RStickX * 0.13 * 0.8;
+    if (CROSS) {
+      lY *= 0.5;
+      lX *= 0.5;
+      rX *= 0.5;
+    }
+    if (OPTION) {
+      lY *= -1;
+      lX *= -1;
+      rX *= -1;
+      Camera.write(180);
+    } else {
+      Camera.write(0);
+    }
     send_data[5] = 30 + (-lY - lX + rX);
     send_data[6] = 30 + (lY - lX + rX);
     send_data[7] = 30 + (lY + lX + rX);
     send_data[8] = 30 + (-lY + lX + rX);
 
-    /*if ( && SWM == 0) {
-        lY*=-1;
-        lX*=-1;
-        rX*=-1;
-        SWM = 1;
-      }
-    else if ( && SWM == 1) {
-        lY*=-1;
-        lX*=-1;
-        rX*=-1;
-        SWM = 0;
-      }*/
 
     //アームの移動
     if (L1)pwm[1] = 100; //上
     else if (L2 > 0)pwm[1] = -100; //下
     else pwm[1] = 0;
-    if (UP && send_data[9] == 0b00001000)pwm[2] = -100; //前
-    else if (DOWN && send_data[9] == 0b00001000)pwm[2] = 100; //後
+    if (UP && send_data[9] == 0b00000100)pwm[2] = -100; //前
+    else if (DOWN && send_data[9] == 0b00000100)pwm[2] = 100; //後
     else pwm[2] = 0;
 
     //アームのつかみ
     if (SQUARE)send_data[9] |= 0b00000001;
     else send_data[9] &= 0b11111110;
-    if (CIRCLE)send_data[9] |= 0b00000010;
-    else send_data[9] &= 0b11111101;
-    if (TRIANGLE)send_data[9] |= 0b00000100;
-    else send_data[9] &= 0b11111011;
-    if (R1)send_data[9] |= 0b00001000;//横
+    if (CIRCLE)send_data[9] |= 0b00001000;
     else send_data[9] &= 0b11110111;
+    if (TRIANGLE)send_data[9] |= 0b00000010;
+    else send_data[9] &= 0b11111101;
+    if (R1)send_data[9] |= 0b00000100;//横
+    else send_data[9] &= 0b11111011;
     //射出
     if (RIGHT)Fire += 0.1;
     if (LEFT)Fire -= 0.1;
+    //task:サーボの向きに合わせて進行方向を反転する。出来れば射出を切り替え方式にする。
     if (CROSS) {
       pwm[3] = 74 * Fire;
       pwm[4] = -74 * Fire;//-73 good
     } else {
       pwm[3] = 0;
       pwm[4] = 0;
+    }
     //装填
-    if (PS4.L1()) {
-      send_data[9] |= 0b00000011;
-    }
-    else {
-      send_data[9] &= 0b00001100;
-    }
+    if (R2 > 0)send_data[10] |= 0b00001100;
+    else send_data[10] &= 0b11110011;
 
     //シリアルモニタに表示
-
+    
     /*****主にいじる所ここまで*****/
-    for (i = 1; i < 5; i++) {//各pwmの比を保ちつつ最大値を超えないように修正してsend_data[1~2]に格納
+    for (i = 1; i <= 4; i++) {//各pwmの比を保ちつつ最大値を超えないように修正してsend_data[1~4]に格納
       double pwm_abs = 0.;//絶対値
       if (pwm[i] > 0)pwm_abs = pwm[i];//pwm[i]の絶対値をpwm_absに代入．
       else pwm_abs = -pwm[i];
       //絶対値が最大値より大きければすべてのpwmに(絶対値/最大値)をかける．
-      if (PWM_MAX < pwm_abs)for (j = 1; j < 5; j++)pwm[j] *= (PWM_MAX / pwm_abs);
+      if (PWM_MAX < pwm_abs)for (j = 1; j <= 4; j++)pwm[j] *= (PWM_MAX / pwm_abs);
       send_data[i] = (int)(pwm[i] + 128);//送信データに代入．
     }
     send_data[0] = '<';
